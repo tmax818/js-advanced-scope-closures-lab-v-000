46a7ebb6dad7aacc9f8a1ef674ebae2bfeb47e7b
Tyler Maxwell <tylermaxwell661@gmail.com>
2018-05-28 11:22:34 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,27 @@
+
+
+function produceDrivingRange(blockRange) {
+  return function (start, finish) {
+    let dist = finish.substr(0, 2) - start.substr(0, 2);
+    if (dist > blockRange) {
+      return `${dist - blockRange} blocks out of range`;
+    } else {
+      return `within range by ${blockRange - dist}`;
+    }
+  };
+}
+
+function produceTipCalculator(tip) {
+  return function (fare) {
+    return fare * tip;
+  };
+}
+
+function createDriver(driverId) {
+  return class Driver {
+      constructor(name, driverId) {
+        this.name = name
+        this.id = driverId++
+      }
+  }
+}
