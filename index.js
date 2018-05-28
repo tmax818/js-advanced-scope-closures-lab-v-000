b93f3c07fba3524f6d44b7302e120d6661f472eb
Tyler Maxwell <tylermaxwell661@gmail.com>
2018-05-28 11:15:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,8 +18,8 @@ function produceTipCalculator(tip) {
 }
 
 function createDriver() {
+  let driverId = 0;
   return class Driver {
-      let driverId = 0;
       constructor(name) {
         this.name = name  
   }
